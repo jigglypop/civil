c461c163795041f4431f93b4124a791b12d9bb10
jigglypop <ydh2244@gmail.com>
2022-05-04 08:20:51 +0900

    Updates

---
+++ src/main.js
@@ -0,0 +1,33 @@
+function factory(el) {
+  const io = new IntersectionObserver(entries => {
+    if (entries.some(entry => entry.intersectionRatio > 0)) {
+      el.style.transform = "translate(0,0)"
+    } else {
+      el.style.transform = "translate(-20rem,0)"
+    }
+  })
+  io.observe(el)
+  return io
+}
+
+const innerItems = document.querySelectorAll('.inner-item')
+innerItems.forEach(innerItem => factory(innerItem))
+
+
+function init() {
+  const title = document.querySelector(".title");
+  const subtitle = document.querySelector(".subtitle");
+  const description = document.querySelector(".description")
+
+  setTimeout(() => {
+    title.classList.add("change");
+      setTimeout(() => {
+        subtitle.classList.add("change");
+            setTimeout(() => {
+              description.classList.add("change");
+            }, 200)
+      }, 200)
+  }, 200)
+}
+
+init()
